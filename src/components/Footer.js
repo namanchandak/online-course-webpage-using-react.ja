ff8025ae843bbf10a510a025445c4024e7d71844
namanchandak <naman.chandak@hotmail.com>
2022-12-23 18:06:10 +0530

    second commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ export default class Footer extends Component {
   <div className="container py-4 py-md-5 px-4 px-md-3">
     <div className="row">
       <div className="col-lg-3 mb-3">
-        <a className="d-inline-flex align-items-center mb-2 link-dark text-decoration-none" href="/" aria-label="Bootstrap">
+        <a className="d-inline-flex align-items-center mb-2 link-dark text-decoration-none" href='#' aria-label="Bootstrap">
           
           <span className="fs-5">Lingua</span>
         </a>
@@ -22,27 +22,27 @@ export default class Footer extends Component {
       <div className="col-6 col-lg-2 offset-lg-1 mb-3">
         <h5>Company</h5>
         <ul className="list-unstyled">
-          <li className="mb-2"><a href="/">Careers</a></li>
-          <li className="mb-2"><a href="/">Teachers</a></li>
-          <li className="mb-2"><a href="/">Support</a></li>
-          <li className="mb-2"><a href="/">Contact</a></li>
+          <li className="mb-2"><a href='#'>Careers</a></li>
+          <li className="mb-2"><a href='#'>Teachers</a></li>
+          <li className="mb-2"><a href='#'>Support</a></li>
+          <li className="mb-2"><a href='#'>Contact</a></li>
           
         </ul>
       </div>
       <div className="col-6 col-lg-2 mb-3">
         <h5>Product</h5>
         <ul className="list-unstyled">
-          <li className="mb-2"><a href="/">Courses</a></li>
-          <li className="mb-2"><a href="/">Pricing</a></li>
-          <li className="mb-2"><a href="/">Blog</a></li>
+          <li className="mb-2"><a href='#'>Courses</a></li>
+          <li className="mb-2"><a href='#'>Pricing</a></li>
+          <li className="mb-2"><a href='#'>Blog</a></li>
           
         </ul>
       </div>
       <div className="col-6 col-lg-2 mb-3">
         <h5>Legal</h5>
         <ul className="list-unstyled">
-          <li className="mb-2"><a href="/">Terms & Conditions</a></li>
-          <li className="mb-2"><a href="/">Privacy policy</a></li>
+          <li className="mb-2"><a href='#'>Terms & Conditions</a></li>
+          <li className="mb-2"><a href='#'>Privacy policy</a></li>
 
 
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar(props) {
 
 <nav className="navbar navbar-expand-lg bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">Lingua</a>
+    <a className="navbar-brand" href='#'>Lingua</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -17,29 +17,29 @@ export default function Navbar(props) {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item"  >
-          <a className="nav-link active" aria-current="page" href="/">Courses</a>
+          <a className="nav-link active" aria-current="page" href='#'>Courses</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">About us</a>
+          <a className="nav-link" href='#'>About us</a>
         </li>
 
         <li className="nav-item">
-          <a className="nav-link" href="/">Teachers</a>
-        </li>
-
-
-        <li className="nav-item">
-          <a className="nav-link" href="/">Pricing</a>
+          <a className="nav-link" href='#'>Teachers</a>
         </li>
 
 
         <li className="nav-item">
-          <a className="nav-link" href="/">Career</a>
+          <a className="nav-link" href='#'>Pricing</a>
+        </li>
+
+
+        <li className="nav-item">
+          <a className="nav-link" href='#'>Career</a>
         </li>
 
 
         <li className="nav-item ">
-          <a className="nav-link" href="/">Blog</a>
+          <a className="nav-link" href='#'>Blog</a>
         </li>
 
         
